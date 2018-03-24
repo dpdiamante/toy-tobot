c20fc7a6841ec9b643b295c2d6385b8097da9a7e
Dominic Diamante <dominic.diamante@gmail.com>
2018-03-24 18:46:26 +1100

    Added Axis class Added class that represents the axis where the robot will be moving.

---
+++ src/main/java/dpd/whispir/robot/toyrobot/domain/model/Axis.java
@@ -0,0 +1,9 @@
+package dpd.whispir.robot.toyrobot.domain.model;
+
+/**
+ * An <code>Axis</code> represents along what axis in the 2 dimensional number plane the robot will be moving.
+ */
+public enum Axis {
+
+    X, Y;
+}
